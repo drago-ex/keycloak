8914f615ce15146ecb589fabd9b8533a9ec481f3
Zdeněk Papučík <zdenek.papucik@gmail.com>
2023-12-08 08:41:24 +0100

    Update Items.php

--- src/Drago/Keycloak/Items.php
+++ src/Drago/Keycloak/Items.php
@@ -10,14 +10,11 @@ declare(strict_types=1);
 namespace Drago\Keycloak;
 
 use League\OAuth2\Client\Token\AccessTokenInterface;
-use Nette\SmartObject;
 use Stevenmaguire\OAuth2\Client\Provider\KeycloakResourceOwner;
 
 
 class Items
 {
-	use SmartObject;
-
 	public function __construct(
 		public ?string $state,
 		public ?AccessTokenInterface $accessToken,
